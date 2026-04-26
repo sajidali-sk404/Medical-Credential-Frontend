e8407551606ba8a620f565ba9d0b7cbccdae122c
Sajid Ali <sajidali.sk404@gmail.com>
2026-04-26 12:00:35 +0500

    fixing production errors

--- src/app/(client)/dashboard/layout.jsx
+++ src/app/(client)/dashboard/layout.jsx
@@ -1,7 +1,7 @@
 // app/(client)/layout.jsx — temporary test
 "use client"
 import { useEffect } from "react"
-import { useAuth } from "@/context/AuthContext"
+import { useAuth } from "../../../../context/AuthContext"
 import { useRouter } from "next/navigation"
 export default function ClientLayout({ children }) {
   const { user, loading, isClient } = useAuth()
